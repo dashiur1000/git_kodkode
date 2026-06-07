97528a0368441f31a1d4a51291443cf4943940fc
david stern <dzs101010@gmail.com>
2026-06-07 15:06:38 +0300

    add week_9

---
+++ week_9/main.py
@@ -0,0 +1,24 @@
+from uuid import main
+
+from fastapi import FastAPI
+import db
+import uvicorn
+
+app = FastAPI()
+
+@app.post("/setup")
+def run_setup():
+    return {"status": "setup triggered"}
+
+@app.get("/schema")
+def get_schema():
+    columns = db.get_schema()
+    return {"columns": columns}
+
+@app.get("/soldiers")
+def ged_all_soldiers():
+    return {"soldiers": []}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
