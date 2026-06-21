57978e2eadb2915cc05d5250af8c5468cb2d42d4
david stern <dzs101010@gmail.com>
2026-06-21 18:33:10 +0300

    day1

---
+++ week_11/Day1/4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Demo;
+class Example4
+{
+    static void Main()
+    {
+        Console.Write("Enter speed: ");
+        string raw = Console.ReadLine();
+        if (double.TryParse(raw, out double speed))
+        {
+            string band;
+            if (speed < 200) band = "slow";
+            else if (speed < 600) band = "cruise";
+            else band = "fast";
+            Console.WriteLine($"Speed {speed} -> {band}");
+        }
+        else
+        {
+            Console.WriteLine("That was not a number.");
+        }
+    }
+}
