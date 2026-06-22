bcfbdd0060d2e46aeeda07d2c38cd58b4b74f7e2
david stern <dzs101010@gmail.com>
2026-06-22 12:50:16 +0300

    Fleet Management Console 1.1

--- week_11/Day2/Fleet Management Console/Program.cs
+++ week_11/Day2/Fleet Management Console/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Thetiger;
+class Fleet
+{
+    static List<string> ids = new List<string>();
+    static List<int> speeds = new List<int>();
+    static List<double> headings = new List<double>();
+
+    static void AddTrack(string id, int speed, double heading)
+    {
+        ids.Add(id);
+        speeds.Add(speed);
+        headings.Add(heading);
+    }
+}
