e6336c3a15684585807acecccf9644cd4a16eae1
david stern <dzs101010@gmail.com>
2026-06-23 12:31:33 +0300

    day 3 part 1

---
+++ week_11/Day3/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Demo;
+class Day3
+{
+    static void TryDouble(int n) { n = n * 2; }
+    static void AddOne(List<int> xn) { xn.Add(1); }
+
+    static void Main()
+    {
+        int x = 10;
+        TryDouble(x);
+        Console.WriteLine(x);
+
+        List<int> list = new List<int>();
+        AddOne(list);
+        AddOne(list);
+        Console.WriteLine(list.Count);
+    }
+}
---
+++ week_11/Day3/2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Demo;
+class Dudi
+{
+    static bool FindSpeed(string id, out double speed)
+    {
+        speed = 0;
+        if (id == "TR-1")
+        { speed = 420.5; return true; }
+        return false;
+    }
+
+    
+        
+    static void Main()
+    {
+        if (FindSpeed("TR-1", out double s))
+            Console.WriteLine($"found: {s}");
+        else
+            Console.WriteLine("not found");
+    }
+}
